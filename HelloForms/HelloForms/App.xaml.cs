069a14a7618b55c3b7e376a4286f8e2a4090e49a
Fadoua Amrou <faitamrou.90@gmail.com>
2020-02-12 15:24:46 -0500

    created a button in xamarin forms

---
+++ HelloForms/HelloForms/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloForms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var btn = new Button
+            {
+                Text = "Click me!"
+            };
+
+            int count = 1;
+
+            btn.Clicked += (sender, args) =>
+            {
+                btn.Text = string.Format("Clicked {0} times.", count);
+                count++;
+            };
+
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        btn
+                    }
+            
+                }
+            };
+
+            //InitializeComponent();
+
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
